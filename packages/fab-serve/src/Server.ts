b0e998c69f32924a40d9bf9d86cbbf71233e6f06
Glen Maddern <glenmaddern@gmail.com>
2018-11-20 18:40:49 +1100

    ready to start porting Erwin's code.

--- packages/fab-serve/src/Server.ts
+++ packages/fab-serve/src/Server.ts
@@ -1,6 +1,18 @@
 export default class Server {
+  private file: string;
+  private port: string;
 
-  static start(file: string, port: string) {
+  constructor(file: string, port: string) {
+    this.file = file;
+    this.port = port;
+  }
 
+  async start() {
+    console.log("Nice.")
+  }
+
+  static async start(file: string, port: string) {
+    const server = new Server(file, port)
+    return await server.start()
   }
 }
