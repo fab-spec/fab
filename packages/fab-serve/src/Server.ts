a490037e7d8b52f024cd8abebddbf7d46511b37c
Glen Maddern <glenmaddern@gmail.com>
2018-11-21 12:15:11 +1100

    finally got my typescript compiling

--- packages/fab-serve/src/Server.ts
+++ packages/fab-serve/src/Server.ts
@@ -1,3 +1,55 @@
+import * as fs from "fs";
+import * as http from "http";
+import * as url from "url";
+import * as vm from "vm";
+import * as path from "path";
+import * as mime from "mime-types";
+import * as fetch from "node-fetch";
+
+const url_parse = url.parse
+const dir = path.resolve(process.argv[2] || './test/fab-dist')
+const file = `${dir}/server/bundle.js`
+const src = fs.readFileSync(file, 'utf8')
+
+const { Request } = fetch
+
+const sandbox = {
+  fetch: fetch,
+  Request: fetch.Request,
+  Response: fetch.Response,
+  Headers: fetch.Headers,
+  URL: URL,
+  console: {
+    log: console.log,
+  },
+  NODE_ENV: 'server',
+  process: {
+    env: {
+      NODE_ENV: 'server',
+    },
+  },
+}
+
+const script = new vm.Script(src)
+const exp = {}
+const ctx = Object.assign({}, sandbox, { module: { exports: exp } })
+const renderer = script.runInNewContext(ctx)
+console.log({ renderer })
+
+const getContentType = (pathname: string) => {
+  const mimeType = mime.lookup(pathname)
+  return mimeType && mime.contentType(mimeType) || 'text/html; charset=utf-8'
+}
+
+const mapToObj = (map: Map<string, string>) => {
+  const obj : {[index: string]: string} = {}
+  return [...map.entries()].reduce(
+    (obj, [key, value]) => ((obj[key] = value), obj),
+    obj
+  )
+}
+
+
 export default class Server {
   private file: string;
   private port: string;
@@ -8,7 +60,44 @@ export default class Server {
   }
 
   async start() {
-    console.log("Nice.")
+    await new Promise((resolve, reject) => {
+      http
+        .createServer(async (req, res) => {
+          try {
+            const pathname = url_parse(req.url!).pathname!
+            if (pathname.startsWith('/_assets')) {
+              fs.readFile(`${dir}${pathname}`, (err, data) => {
+                res.setHeader('Content-Type', getContentType(pathname))
+                res.end(data)
+              })
+            } else {
+              const method = req.method
+              const headers: any = req.headers
+              const url = `https://${req.headers.host}${req.url}`
+              const fetch_req = new Request(url, {
+                method,
+                headers,
+              })
+              const fetch_res = await renderer.render(fetch_req, {
+                injected: 'variables',
+                should: 'work!',
+              })
+              console.log({fetch_res})
+              res.writeHead(
+                fetch_res.status,
+                fetch_res.statusText,
+                mapToObj(fetch_res.headers)
+              )
+              const blob = await fetch_res.arrayBuffer()
+              res.write(new Buffer(blob))
+              res.end()
+            }
+          } catch (e) {
+            reject(e)
+          }
+        })
+        .listen(this.port, resolve);
+    })
   }
 
   static async start(file: string, port: string) {
