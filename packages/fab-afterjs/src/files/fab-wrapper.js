da06a1e2652619caa6b3c4efcd58ce71102f10c1
Glen Maddern <glenmaddern@gmail.com>
2018-11-29 18:15:17 +1100

    converting global request URLs into paths only as AfterJS expects

--- packages/fab-afterjs/src/files/fab-wrapper.js
+++ packages/fab-afterjs/src/files/fab-wrapper.js
@@ -8,10 +8,27 @@
 
 const MockExpressResponse = require('./mock-express-response')
 const render_app = require('app-index').default
+const url = require('url')
 
 const render = async (req, settings) => {
   const res = new MockExpressResponse()
-  const body = await render_app(req, res, settings)
+  const req_url = url.parse(req.url)
+  const { path } = req_url
+
+  if (path.startsWith('/static') || path.endsWith('.ico') || path.endsWith('.txt')) {
+    return new Response(null, {
+      status: 302,
+      headers: {
+        Location: `/_assets${path}`
+      }
+    })
+  }
+
+  const local_req = new Request(path, {
+    method: req.method,
+    headers: req.headers
+  })
+  const body = await render_app(local_req, res, settings)
   return new Response(body, {
     status: res.statusCode,
     headers: res._headers
