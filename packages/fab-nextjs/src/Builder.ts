ede5a4ce68abc4317c2777f92b7bd96e9e781acc
Glen Maddern <glenmaddern@gmail.com>
2018-12-11 22:34:55 +1100

    time to start porting the proof-of-concept nextjs stuff

--- packages/fab-nextjs/src/Builder.ts
+++ packages/fab-nextjs/src/Builder.ts
@@ -17,13 +17,14 @@ export default class Builder {
     output_file: string,
     intermediate_only: boolean
   ) {
-    if (!(await fs.pathExists(dir))) {
+    const next_dir = path.join(dir, '.next')
+    if (!(await fs.pathExists(dir)) || !(await fs.pathExists(next_dir))) {
       error(`${dir} doesn't exist!`)
       throw new Error('Missing directory')
     }
 
     log(`Building ${chalk.green(dir)}`)
-    const assets_path = path.join(dir, '.next', 'build-manifest.json')
+    const assets_path = path.join(next_dir, 'build-manifest.json')
 
     if (!(await fs.pathExists(assets_path))) {
       error(`Missing build manifest in ${assets_path}.`)
@@ -33,5 +34,25 @@ export default class Builder {
 
       throw new Error('Missing .next/build-manifest.json')
     }
+
+    await fs.emptyDir(working_dir)
+    const int_dir = path.join(working_dir, 'intermediate')
+    await fs.ensureDir(path.join(int_dir, '_assets'))
+    await fs.ensureDir(path.join(int_dir, '_server'))
+
+    console.log(`Copying .next/static to .fab/intermediate/static`)
+    await fs.copy(
+      path.join(next_dir, 'static'),
+      path.join(int_dir, 'static')
+    )
+
+    if (intermediate_only)
+      return console.log(`--intermediate-only set. Stopping here.`)
+
+    await Compiler.compile(
+      int_dir,
+      path.join(working_dir, 'build'),
+      output_file
+    )
   }
 }
