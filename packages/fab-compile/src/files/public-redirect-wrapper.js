d84663f1c38ec28c005e33b0a6f792a1f3563df6
Glen Maddern <glenmaddern@gmail.com>
2019-01-09 13:40:14 +1100

    add content-type if not already present

--- packages/fab-compile/src/files/public-redirect-wrapper.js
+++ packages/fab-compile/src/files/public-redirect-wrapper.js
@@ -10,6 +10,12 @@ const url = require('url')
 const render_app = require('app-index').render
 const production_settings = require('production-settings.json')
 const rewrites = FAB_REWRITES
+const mime = require('mime-types')
+
+const getContentType = (pathname) => {
+  const mimeType = mime.lookup(pathname)
+  return (mimeType && mime.contentType(mimeType)) || 'text/html; charset=utf-8'
+}
 
 const render = async (req, settings) => {
   const parsed = url.parse(req.url)
@@ -22,6 +28,9 @@ const render = async (req, settings) => {
     // Delete cache control & return response
     const headers = Object.assign({}, response.headers)
     delete headers['cache-control']
+
+    // Add mime types if not already present
+    if (!headers['content-type']) headers['content-type'] = getContentType(path)
 
     return new Response(await response.body, {
       status: response.status,
