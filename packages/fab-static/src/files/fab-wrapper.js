b32ea3e500a07b479ffc66f8a36e7314baa0af81
Glen Maddern <glenmaddern@gmail.com>
2019-02-05 11:16:42 +1100

    @fab/static v0.1.0-5: removing HOST and rewriting ORIGIN header on proxied requests

--- packages/fab-static/src/files/fab-wrapper.js
+++ packages/fab-static/src/files/fab-wrapper.js
@@ -5,7 +5,7 @@
  *
  * */
 
-import url from 'url'
+import { parse } from 'url'
 import htmls from './htmls'
 import * as default_server from './default-app-server'
 import * as server_overrides from 'app-server'
@@ -23,7 +23,7 @@ export const render = async (request, settings) => {
 }
 
 async function _render(request, settings) {
-  const req_url = url.parse(request.url)
+  const req_url = parse(request.url)
 
   const route = await SERVER.route(settings, req_url.path, request)
 
@@ -34,7 +34,7 @@ async function _render(request, settings) {
   if (typeof route !== 'string') return route
 
   // Routing to an absolute URL we'll proxy the request
-  const parsed_route = url.parse(route)
+  const parsed_route = parse(route)
   if (parsed_route.hostname) return proxyRequest(request, route)
 
   const path = parsed_route.path
@@ -64,8 +64,15 @@ async function _render(request, settings) {
 }
 
 function proxyRequest(request, url) {
+  const { protocol, host } = parse(url)
+
+  // Ensure host and origin headers are set appropriately
+  const headers = new Headers(request.headers)
+  headers.delete('host')
+  if (headers.get('origin')) headers.set('origin', `${protocol}//${host}`)
+
   return fetch(url, {
-    headers: request.headers,
+    headers,
     method: request.method,
     ...(request.method === 'POST' ? { body: request.body } : {})
   })
