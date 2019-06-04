d2235397c78467908aee42134bab31790e5f7f32
Glen Maddern <glenmaddern@gmail.com>
2019-06-04 16:52:29 +0200

    re-prettifying the afterjs file

--- packages/fab-afterjs/src/files/fab-wrapper.js
+++ packages/fab-afterjs/src/files/fab-wrapper.js
@@ -6,24 +6,24 @@
  *
  * */
 
-const MockExpressResponse = require("./mock-express-response");
-const render_app = require("./app").default;
-const url = require("url");
+const MockExpressResponse = require('./mock-express-response')
+const render_app = require('./app').default
+const url = require('url')
 
 const render = async (req, settings) => {
-  const res = new MockExpressResponse();
-  const req_url = url.parse(req.url);
-  const { path } = req_url;
+  const res = new MockExpressResponse()
+  const req_url = url.parse(req.url)
+  const { path } = req_url
 
   const local_req = new Request(path, {
     method: req.method,
     headers: req.headers
-  });
-  const body = await render_app(local_req, res, settings);
+  })
+  const body = await render_app(local_req, res, settings)
   return new Response(body, {
     status: res.statusCode,
     headers: res._headers
-  });
-};
+  })
+}
 
-module.exports = { render };
+module.exports = { render }
