8632b9b1a26af01d681fe31cc3a52f1183c1bca0
Kat <dmitry@snoculars.com>
2019-08-31 21:50:17 -0400

    Remove console.log statment
    Removing the console.log statement in the wrangler template

--- packages/fab-cf-workers/src/files/wrangler-template/index.js
+++ packages/fab-cf-workers/src/files/wrangler-template/index.js
@@ -6,7 +6,6 @@ const handleRequest = async (request) => {
   const url = new URL(request.url)
   if (url.pathname.startsWith('/_assets/')) {
     const asset_url = asset_base_url + url.pathname
-    console.log({asset_url})
     return await fetch(asset_url)
   } else {
     let settings = fab.getProdSettings ? fab.getProdSettings() : {}
