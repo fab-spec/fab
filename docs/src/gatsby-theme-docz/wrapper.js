077d2d7f7b02f65e1900f9b5d2f01bd8eff52472
rakannimer <rakannimer@gmail.com>
2019-10-21 16:19:31 -0500

    add favicon

---
+++ docs/src/gatsby-theme-docz/wrapper.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { Helmet } from 'react-helmet'
+
+const Wrapper = ({ children }) => (
+  <React.Fragment>
+    <Helmet>
+      <link rel="icon" type="image/png" href="/public/favicon-32x32.png" />
+    </Helmet>
+    {children}
+  </React.Fragment>
+)
+export default Wrapper
