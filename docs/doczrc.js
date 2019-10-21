f847d3dbd3bba813eea0e53114dbc0ec88d74afb
rakannimer <rakannimer@gmail.com>
2019-10-21 17:32:13 -0500

    remove deprecated config fields

--- docs/doczrc.js
+++ docs/doczrc.js
@@ -2,17 +2,4 @@ export default {
   title: 'FAB — Frontend Application Bundles',
   description: 'Homepage for the FAB project & specification',
   files: ['../packages/*/README.md', './**/*.mdx', '../README.md'],
-  themeConfig: {
-    linesToScrollEditor: 999999,
-    showPlaygroundEditor: false,
-    codeSandbox: false,
-    logo: {
-      src:
-        'https://user-images.githubusercontent.com/23264/53997173-49745400-418f-11e9-87d0-60a9da6449e6.png',
-      width: '100%',
-    },
-  },
-  htmlContext: {
-    favicon: '/public/favicon-32x32.png',
-  },
 }
