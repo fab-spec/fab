90b8cbd1a434b9d58d94faf35c3091096b9ec8d4
Glen Maddern <glenmaddern@gmail.com>
2019-11-18 20:12:55 +0000

    building a FAB inside the new CRA!

--- tests/e2e/create-react-app.test.ts
+++ tests/e2e/create-react-app.test.ts
@@ -17,7 +17,7 @@ it('should allow creation of a tmp dir', async () => {
   await shell(`ls -l ${dir.path}`)
   const { stdout } = await cmd(`pwd`, { cwd: dir.path })
   console.log({ stdout })
-  expect(stdout).toMatch(/tmp/)
+  expect(stdout).toMatch('tmp')
 })
 
 
@@ -25,7 +25,13 @@ it('should allow creation of a new CRA project', async () => {
   const dir = await tmp.dir({ dir: process.env.GITHUB_WORKSPACE })
   await shell(`ls -l ${dir.path}`)
   await shell(`yarn create react-app cra-test`, { cwd: dir.path })
-  const { stdout: files } = await cmd(`ls -l ${dir.path}/cra-test`)
-  console.log(files)
-  expect(files).toMatch(/package\.json/)
+  const cwd = `${dir.path}/cra-test`;
+  const { stdout: files } = await cmd(`ls -l ${cwd}`)
+  expect(files).toMatch('package.json')
+
+  await shell(`yarn build`, {cwd})
+  await shell(`fab-static build`, {cwd})
+
+  const { stdout: files_after_fab_build } = await cmd(`ls -l ${cwd}`)
+  expect(files_after_fab_build).toMatch('fab.zip')
 })
