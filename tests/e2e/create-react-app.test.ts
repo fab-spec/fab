d3372b9ace64ca8584769f856432555273491936
Glen Maddern <glenmaddern@gmail.com>
2019-11-18 18:43:58 +0000

    See if we can generate a tmp dir

---
+++ tests/e2e/create-react-app.test.ts
@@ -0,0 +1,11 @@
+import * as execa from 'execa'
+import * as tmp from 'tmp-promise'
+
+it('should have our packages locally', async () => {
+  const dir = await tmp.dir()
+  console.log({dir})
+  await execa.command(`cd ${dir}`)
+  const { stdout } = await execa.command(`pwd`)
+  console.log({stdout})
+  expect(stdout).toMatch(/tmp/)
+})
