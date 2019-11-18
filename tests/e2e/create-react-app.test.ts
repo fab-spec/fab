c433296c22f8a42b313d5b1458fa042fcb262b67
Glen Maddern <glenmaddern@gmail.com>
2019-11-18 18:53:59 +0000

    attempting to create the tmp dir inside GITHUB_WORKSPACE

--- tests/e2e/create-react-app.test.ts
+++ tests/e2e/create-react-app.test.ts
@@ -1,8 +1,8 @@
 import * as execa from 'execa'
 import * as tmp from 'tmp-promise'
 
-it('should have our packages locally', async () => {
-  const dir = await tmp.dir()
+it('should allow creation of a tmp dir', async () => {
+  const dir = await tmp.dir({dir: process.env.GITHUB_WORKSPACE})
   console.log({dir})
   await execa.command(`cd ${dir.path}`)
   const { stdout } = await execa.command(`pwd`)
