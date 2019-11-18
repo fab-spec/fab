dcc81f9097a288061be430b7b9214855c3cbd6f4
Glen Maddern <glenmaddern@gmail.com>
2019-11-18 18:08:13 +0000

    got a global typescript test executing

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/', '/packages/'],
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tests/tsconfig.json'
+    }
+  }
+};
---
+++ tests/e2e/hello-world.test.ts
@@ -0,0 +1,11 @@
+import * as execa from 'execa'
+
+it('should pass a noop', () => {
+  expect(1+1).toBe(2)
+})
+
+it('should have our packages locally', async () => {
+  const { stdout: bins } = await execa.command(`ls -l node_modules/.bin`)
+  console.log({bins})
+  expect(bins).toMatch(/fab-static/)
+})
