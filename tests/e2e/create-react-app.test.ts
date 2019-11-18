78e48e1aa24acf060f6d628485b42c69dba9a965
Glen Maddern <glenmaddern@gmail.com>
2019-11-18 19:08:55 +0000

    ok smarty, lets try yarn create react-app!

--- tests/e2e/create-react-app.test.ts
+++ tests/e2e/create-react-app.test.ts
@@ -1,11 +1,29 @@
 import * as execa from 'execa'
 import * as tmp from 'tmp-promise'
 
+const cmd = (command: string, ...opts: any) => {
+  console.log(command)
+  return execa.command(command, ...opts)
+}
+const shell = async (command: string, ...opts: any) => {
+  console.log((await cmd(command)).stdout)
+}
+
 it('should allow creation of a tmp dir', async () => {
   const dir = await tmp.dir({ dir: process.env.GITHUB_WORKSPACE })
   console.log({ dir })
-  console.log((await execa.command(`ls -l ${dir.path}`)).stdout)
-  const { stdout } = await execa.command(`pwd`, { cwd: dir.path })
+  await shell(`ls -l ${dir.path}`)
+  const { stdout } = await cmd(`pwd`, { cwd: dir.path })
   console.log({ stdout })
   expect(stdout).toMatch(/tmp/)
+})
+
+
+it('should allow creation of a new CRA project', async () => {
+  const dir = await tmp.dir({ dir: process.env.GITHUB_WORKSPACE })
+  await shell(`ls -l ${dir.path}`)
+  await shell(`yarn create react-app .`, { cwd: dir.path })
+  const { stdout: files } = await cmd(`ls -l ${dir.path}`)
+  console.log(files)
+  expect(files).toMatch(/package\.json/)
 })
