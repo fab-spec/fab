1bc619cc34510cb65d7f48bfca9f381986f4f8bb
Glen Maddern <glenmaddern@gmail.com>
2019-11-18 18:57:05 +0000

    testing if cd and pwd with cwd works on GH

--- tests/e2e/create-react-app.test.ts
+++ tests/e2e/create-react-app.test.ts
@@ -2,10 +2,10 @@ import * as execa from 'execa'
 import * as tmp from 'tmp-promise'
 
 it('should allow creation of a tmp dir', async () => {
-  const dir = await tmp.dir({dir: process.env.GITHUB_WORKSPACE})
-  console.log({dir})
-  await execa.command(`cd ${dir.path}`)
-  const { stdout } = await execa.command(`pwd`)
-  console.log({stdout})
+  const dir = await tmp.dir({ dir: process.env.GITHUB_WORKSPACE })
+  console.log({ dir })
+  console.log((await execa.command(`cd ${dir.path}`)).stdout)
+  const { stdout } = await execa.command(`pwd`, { cwd: dir.path })
+  console.log({ stdout })
   expect(stdout).toMatch(/tmp/)
 })
