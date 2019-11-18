f1d66fb6f79bd7961af002c69f69433368fe94d3
Glen Maddern <glenmaddern@gmail.com>
2019-11-18 18:45:40 +0000

    use the tmp dir properly

--- tests/e2e/create-react-app.test.ts
+++ tests/e2e/create-react-app.test.ts
@@ -4,7 +4,7 @@ import * as tmp from 'tmp-promise'
 it('should have our packages locally', async () => {
   const dir = await tmp.dir()
   console.log({dir})
-  await execa.command(`cd ${dir}`)
+  await execa.command(`cd ${dir.path}`)
   const { stdout } = await execa.command(`pwd`)
   console.log({stdout})
   expect(stdout).toMatch(/tmp/)
