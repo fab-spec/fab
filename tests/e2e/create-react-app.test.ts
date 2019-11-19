6f793bbfcab0abadaf36e56277fc5f405cc72626
Glen Maddern <glenmaddern@gmail.com>
2019-11-19 14:29:53 +0000

    rolling back to the setup I had last night

--- tests/e2e/create-react-app.test.ts
+++ tests/e2e/create-react-app.test.ts
@@ -2,7 +2,6 @@ import * as execa from 'execa'
 import * as tmp from 'tmp-promise'
 import * as fs from 'fs-extra'
 import * as expect from 'expect'
-import * as path from 'path'
 
 const cmd = (command: string, ...opts: any) => {
   process.stdout.write(`$ ${command}\n`)
@@ -24,21 +23,17 @@ it('should allow creation of a tmp dir', async () => {
 })
 
 it('should allow creation of a new CRA project', async () => {
-  const dir = await tmp.dir({
-    dir: process.env.GITHUB_WORKSPACE ? process.env.HOME : undefined,
-  })
-  const root = process.env.GITHUB_WORKSPACE || path.resolve('.')
-  console.log({root})
-
+  const dir = await tmp.dir({ dir: process.env.GITHUB_WORKSPACE })
   await shell(`ls -l ${dir.path}`)
   await shell(`yarn create react-app cra-test`, { cwd: dir.path })
   const cwd = `${dir.path}/cra-test`
   const { stdout: files } = await cmd(`ls -l ${cwd}`)
   expect(files).toMatch('package.json')
 
+  await fs.writeFile(`${cwd}/.env`, `SKIP_PREFLIGHT_CHECK=true`)
+  await shell(`cat .env`, { cwd })
   await shell(`yarn build`, { cwd })
-  await shell(`yarn add link:${root}/packages/fab-static`, { cwd })
-  await shell(`yarn run fab-static build`, { cwd })
+  await shell(`fab-static build`, { cwd })
 
   const { stdout: files_after_fab_build } = await cmd(`ls -l ${cwd}`)
   expect(files_after_fab_build).toMatch('fab.zip')
