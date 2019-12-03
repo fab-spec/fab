ef07c31148516bf27ad40f343b42507dafbf9abd
Glen Maddern <glenmaddern@gmail.com>
2019-12-03 17:25:07 +0000

    ugh why did you suggest that then?

--- packages/cli/test/commands/build.test.ts
+++ packages/cli/test/commands/build.test.ts
@@ -4,7 +4,7 @@ describe('build', () => {
   test
     .stdout()
     .command(['build'])
-    .catch(err => expect(err.message).to.be(`Missing config file at 'fab.config.json5'`))
+    .catch(err => expect(err.message).to.equal(`Missing config file at 'fab.config.json5'`))
     .it(`should report that it can't find the default config file`, ctx => {
       expect(ctx.stdout).to.contain('All FAB tooling assumes that you have a valid config file')
       expect(ctx.stdout).to.contain('use the --config argument')
@@ -14,7 +14,7 @@ describe('build', () => {
   test
     .stdout()
     .command(['build', '--config', 'no-existo.json5'])
-    .catch(err => expect(err.message).to.be(`Missing config file at 'no-existo.json5'`))
+    .catch(err => expect(err.message).to.equal(`Missing config file at 'no-existo.json5'`))
     .it('should report that the specified config file is missing', ctx => {
       expect(ctx.stdout).to.contain(`Config file 'no-existo.json5' not found.`)
       expect(ctx.stdout).to.contain('fab init')
