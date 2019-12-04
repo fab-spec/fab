a155a4c8de8fe5820a37c9476f828765b6d5f9cf
Glen Maddern <glenmaddern@gmail.com>
2019-12-04 12:39:25 +0000

    fixing chai's lack of async testing

--- packages/input-static/test/build.test.ts
+++ packages/input-static/test/build.test.ts
@@ -1,13 +1,25 @@
 import plugin from '../src'
-import { expect } from 'chai';
-import {ProtoFab} from "@fab/core";
+import { expect } from 'chai'
+import { ProtoFab } from '@fab/core'
+
+const shouldThrow = async (async_fn: () => any, message: string) => {
+  try {
+    await async_fn()
+    throw new Error('Should have thrown!')
+  } catch (err) {
+    expect(err.message).to.equal(message)
+  }
+}
 
 describe('@fab/input-static', () => {
   it('should be a function', () => {
     expect(plugin.build).to.be.a('function')
   })
 
-  it('should require a dir argument', () => {
-    expect(() => plugin.build({}, new ProtoFab())).to.throw('Config file contains errors!')
+  it('should require a dir argument', async () => {
+    await shouldThrow(
+      () => plugin.build({}, new ProtoFab()),
+      'Config file contains errors!'
+    )
   })
 })
