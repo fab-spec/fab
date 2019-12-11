7f8d0275b85314031aa69c8e2572c7098c97a156
Glen Maddern <glenmaddern@gmail.com>
2019-12-11 14:01:37 +0000

    got a webpack build working and figured out my trouble with rollup was a plugin problem not a ESM one... maybe?

--- packages/cli/src/actions/Compiler.ts
+++ packages/cli/src/actions/Compiler.ts
@@ -1,6 +1,9 @@
 import fs from 'fs-extra'
 import { PluginMetadata, ProtoFab } from '@fab/core'
 import { rollup } from 'rollup'
+import webpack from 'webpack'
+import * as path from 'path'
+import resolve from '@rollup/plugin-node-resolve'
 
 export class Compiler {
   static async compile(proto_fab: ProtoFab<PluginMetadata>, render_plugins: [string?]) {
@@ -12,7 +15,7 @@ export class Compiler {
 ${render_plugins
   .map(
     (plugin, i) => `
-import { render as renderer_${i} } from '${plugin}'
+import { render as renderer_${i} } from '${plugin}/render'
 `
   )
   .join('\n')}
@@ -25,9 +28,38 @@ const renderers = [
     await fs.writeFile('.fab/build/server.js', server_contents)
 
     const bundle = await rollup({
-      input: '.fab/build/server.js',
+      input: './.fab/build/server.js',
+      plugins: [resolve()],
     })
     const { output } = await bundle.generate({ format: 'esm' })
     console.log(output)
+
+    // await new Promise((resolve, reject) =>
+    //   webpack(
+    //     {
+    //       mode: 'production',
+    //       target: 'webworker',
+    //       entry: './.fab/build/server.js',
+    //       output: {
+    //         path: path.resolve('.fab/built'),
+    //         filename: 'server.js',
+    //         library: 'server',
+    //         libraryTarget: 'commonjs2',
+    //       },
+    //       optimization: {
+    //         minimize: false
+    //       },
+    //     },
+    //     (err, stats) => {
+    //       if (err || stats.hasErrors()) {
+    //         console.log('Build failed.')
+    //         console.log(err)
+    //         console.log(stats.toJson().errors.toString())
+    //         reject()
+    //       }
+    //       resolve()
+    //     }
+    //   )
+    // )
   }
 }
