2c95aa4e0bd23b3038f8038996d8f2c5cdb219b9
Glen Maddern <glenmaddern@gmail.com>
2019-12-12 15:54:30 +0000

    and some more

--- tests/e2e/static.test.ts
+++ tests/e2e/static.test.ts
@@ -2,57 +2,58 @@ import * as tmp from 'tmp-promise'
 import { expectError, shell } from '../utils'
 
 describe('dir of static assets', () => {
-  let cwd: string
+  const opts = { cwd: '' }
 
   beforeAll(async () => {
     const tmp_dir = await tmp.dir({ dir: process.env.GITHUB_WORKSPACE })
-    cwd = `${tmp_dir.path}/static`
-    await shell(`cp -R ${__dirname}/static ${cwd}`)
+    opts.cwd = `${tmp_dir.path}/static`
+    await shell(`cp -R ${__dirname}/static ${opts.cwd}`)
   })
 
   describe('failure cases', () => {
     it('should handle a missing config file', async () => {
-      const no_config = await expectError(`fab build`, { cwd })
-      expect(no_config.stderr).toContain(`Error: Missing config file`)
-      expect(no_config.stderr).toContain(`fab.config.json5`)
-      expect(no_config.stdout).toContain(
+      const { stderr, stdout } = await expectError(`fab build`, opts)
+      expect(stderr).toContain(`Error: Missing config file`)
+      expect(stderr).toContain(`fab.config.json5`)
+      expect(stdout).toContain(
         `All FAB tooling assumes that you have a valid config file`
       )
-      expect(no_config.stdout).toContain(`fab.config.json5`)
+      expect(stdout).toContain(`fab.config.json5`)
     })
 
     it('should handle an empty config file', async () => {
-      const empty_config = await expectError(`fab build -c fab.empty-config.json5`, {
-        cwd,
-      })
-      expect(empty_config.stderr).toContain(
+      const { stderr, stdout } = await expectError(
+        `fab build -c fab.empty-config.json5`,
+        opts
+      )
+      expect(stderr).toContain(
         `Error: The FAB config file is missing a 'build' property.`
       )
-      expect(empty_config.stdout).toContain(`Config file contains errors!`)
+      expect(stdout).toContain(`Config file contains errors!`)
     })
 
     it(`should tell you if you reference a module it can't find`, async () => {
-      const unknown_module = await expectError(`fab build -c fab.unknown-module.json5`, {
-        cwd,
-      })
-      expect(unknown_module.stderr).toContain(`Cannot find module '@fab/no-existo'`)
-      expect(unknown_module.stderr).toContain(`Are you sure it's installed?`)
-      expect(unknown_module.stdout).toContain(`Config file contains errors!`)
+      const { stderr, stdout } = await expectError(
+        `fab build -c fab.unknown-module.json5`,
+        opts
+      )
+      expect(stderr).toContain(`Cannot find module '@fab/no-existo'`)
+      expect(stderr).toContain(`Are you sure it's installed?`)
+      expect(stdout).toContain(`Config file contains errors!`)
     })
 
     it(`should tell you you've forgotten @fab/rewire-assets`, async () => {
-      const missing_rewire = await expectError(`fab build -c fab.missing-rewire.json5`, {
-        cwd,
-      })
-      expect(missing_rewire.stdout).toContain(`Build failed!`)
-      expect(missing_rewire.stdout).toContain(
+      const { stderr, stdout } = await expectError(
+        `fab build -c fab.missing-rewire.json5`,
+        opts
+      )
+      expect(stdout).toContain(`Build failed!`)
+      expect(stdout).toContain(
         `Build config leaves files outside of _assets dir: index.html`
       )
-      expect(missing_rewire.stdout).toContain(`You might need to add @fab/rewire-assets`)
-      expect(missing_rewire.stdout).toContain(
-        `See https://fab.dev/packages/rewire-assets`
-      )
-      expect(missing_rewire.stderr).toContain(`Build failed!`)
+      expect(stdout).toContain(`You might need to add @fab/rewire-assets`)
+      expect(stdout).toContain(`See https://fab.dev/packages/rewire-assets`)
+      expect(stderr).toContain(`Build failed!`)
     })
   })
 })
