179d0c78ed647ac65621c7de98079ae4de37a33f
Glen Maddern <glenmaddern@gmail.com>
2019-12-12 15:02:39 +0000

    FANTASTIC PROGRESS, we have Rollup generating a FAB-compatible IIFE & a fab.zip THAT WORKS!

--- packages/cli/src/actions/Compiler.ts
+++ packages/cli/src/actions/Compiler.ts
@@ -1,11 +1,18 @@
 import fs from 'fs-extra'
-import { PluginMetadata, ProtoFab } from '@fab/core'
+import { PluginMetadata, ProtoFab, log } from '@fab/core'
 import { rollup } from 'rollup'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import json from '@rollup/plugin-json'
+import util from 'util'
+
+// @ts-ignore
+import _zip from 'deterministic-zip'
+const zip = util.promisify(_zip)
+
 // @ts-ignore
 import hypothetical from 'rollup-plugin-hypothetical'
+import * as path from 'path'
 
 export class Compiler {
   static async compile(proto_fab: ProtoFab<PluginMetadata>, render_plugins: string[]) {
@@ -27,8 +34,6 @@ const renderers = [
 ]
 `
 
-    // await fs.writeFile('.fab/build/server.js', server_contents)
-
     const bundle = await rollup({
       input: '@fab/cli/lib/runtime',
       plugins: [
@@ -43,8 +48,30 @@ const renderers = [
         json(),
       ],
     })
-    const { output } = await bundle.generate({ format: 'esm' })
+    const {
+      output: [output, ...chunks],
+    } = await bundle.generate({ format: 'iife' })
     console.log(output)
+
+    if (chunks.length > 0) {
+      log.error(`WARNING: Didn't expect there to be more than one chunk created! Got:`)
+      console.log(chunks)
+    }
+    await fs.writeFile('.fab/build/server.js', output.code)
+
+    console.log(`Zipping it up into a FAB`)
+    const zipfile = path.resolve('fab.zip')
+    const build_dir = path.resolve('.fab/build')
+    const options = {
+      includes: ['./server.js', './_assets/**'],
+      cwd: build_dir,
+    }
+    await zip(build_dir, zipfile, options)
+    const stats2 = await fs.stat(zipfile)
+    console.log(
+      `    ${path.relative(process.cwd(), zipfile)} (${Math.round(stats2.size / 1024) +
+        'KB'})`
+    )
 
     // await new Promise((resolve, reject) =>
     //   webpack(
--- packages/cli/src/runtime/index.ts
+++ packages/cli/src/runtime/index.ts
@@ -4,6 +4,7 @@ import { FabSettings, FabSpecRender } from '@fab/core'
 
 export const render: FabSpecRender = (request: Request, settings: FabSettings) => {
   console.log(request.url)
+  console.log("It's definitely being called, Glen.")
 
   return new Response(null, {
     status: 404,
