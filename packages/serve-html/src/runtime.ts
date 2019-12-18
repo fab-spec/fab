26b37e0daab576f63174968b8d468294f1b42a14
Glen Maddern <glenmaddern@gmail.com>
2019-12-18 22:18:39 +0000

    starting the runtime

--- packages/serve-html/src/runtime.ts
+++ packages/serve-html/src/runtime.ts
@@ -5,6 +5,9 @@ export const runtime: FabPluginRuntime<ServeHtmlArgs, ServeHtmlMetadata> = (
   args: ServeHtmlArgs,
   metadata: ServeHtmlMetadata
 ) => {
+  const htmls = metadata.serve_html.htmls
+  console.log({ htmls })
+
   return async function({}) {
     return undefined
   }
