c7f0cc889a4001b47572c3a1154e1afbfbf2d96a
rakannimer <rakannimer@gmail.com>
2019-12-18 17:10:33 +0200

    create readmes

--- docs/scripts/build.js
+++ docs/scripts/build.js
@@ -2,6 +2,10 @@ const glob = require('fast-glob')
 const path = require('path')
 const { copyHomePackageReadmeToDocs, copyPackageReadmeToDocs } = require('./utils')
 
+try {
+  fs.mkdirSync(path.resolve(__dirname, `../readmes`))
+} catch (err) {}
+
 const entries = glob.sync('packages/*/README.md')
 for (const entry of entries) {
   copyPackageReadmeToDocs(entry)
--- docs/scripts/watch.js
+++ docs/scripts/watch.js
@@ -2,6 +2,10 @@ const chokidar = require('chokidar')
 const path = require('path')
 const { copyHomePackageReadmeToDocs, copyPackageReadmeToDocs } = require('./utils')
 
+try {
+  fs.mkdirSync(path.resolve(__dirname, `../readmes`))
+} catch (err) {}
+
 chokidar
   .watch('packages/*/README.md', { cwd: path.resolve(__dirname, '../../') })
   .on('all', (event, source) => {
