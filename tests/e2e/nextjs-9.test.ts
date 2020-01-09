6c8040c5f567d756177ea6f9963627af05931f20
Glen Maddern <glenmaddern@gmail.com>
2020-01-09 12:06:42 +0000

    stubbing out a nextjs9 e2e test

--- tests/e2e/nextjs-9.test.ts
+++ tests/e2e/nextjs-9.test.ts
@@ -2,22 +2,14 @@ import * as tmp from 'tmp-promise'
 import * as fs from 'fs-extra'
 import { shell, cmd } from '../utils'
 
-it('should allow creation of a tmp dir', async () => {
+it('should allow creation of a new NextJS project into a FAB', async () => {
   const dir = await tmp.dir({ dir: process.env.GITHUB_WORKSPACE })
   await shell(`ls -l ${dir.path}`)
-  const { stdout } = await cmd(`pwd`, { cwd: dir.path })
-  expect(stdout).toMatch('tmp')
-})
-
-it('should allow creation of a new CRA project into a FAB', async () => {
-  const dir = await tmp.dir({ dir: process.env.GITHUB_WORKSPACE })
-  await shell(`ls -l ${dir.path}`)
-  await shell(`yarn create react-app cra-test`, { cwd: dir.path })
-  const cwd = `${dir.path}/cra-test`
+  await shell(`yarn create next-app nextjs9-test`, { cwd: dir.path })
+  const cwd = `${dir.path}/next-app`
   const { stdout: files } = await cmd(`ls -l ${cwd}`)
   expect(files).toMatch('package.json')
 
-  await fs.writeFile(`${cwd}/.env`, `SKIP_PREFLIGHT_CHECK=true`)
   await shell(`cat .env`, { cwd })
   await shell(`fab init -y --skip-install --version=next`, { cwd })
   await shell(`yarn build:fab`, { cwd })
