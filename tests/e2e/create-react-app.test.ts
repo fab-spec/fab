89e250ffba4905a4e27c3475ea83b223d89c5409
Glen Maddern <glenmaddern@gmail.com>
2020-01-21 21:30:02 +0000

    trying to test this properly, not working locally...

--- tests/e2e/create-react-app.test.ts
+++ tests/e2e/create-react-app.test.ts
@@ -1,9 +1,12 @@
 import * as tmp from 'tmp-promise'
 import * as fs from 'fs-extra'
 import { shell, cmd } from '../utils'
+import * as execa from 'execa'
+import { ExecaChildProcess } from 'execa'
 
 describe('Create React App E2E Test', () => {
   let tmpdir: string
+  let cwd: string
 
   it('should allow creation of a tmp dir', async () => {
     const dir = await tmp.dir({ dir: process.env.GITHUB_WORKSPACE })
@@ -16,30 +19,22 @@ describe('Create React App E2E Test', () => {
   it('should allow creation of a new CRA project into a FAB', async () => {
     await shell(`ls -l ${tmpdir}`)
     await shell(`yarn create react-app cra-test`, { cwd: tmpdir })
-    const cwd = `${tmpdir}/cra-test`
+    cwd = `${tmpdir}/cra-test`
     const { stdout: files } = await cmd(`ls -l ${cwd}`)
     expect(files).toMatch('package.json')
 
     await fs.writeFile(`${cwd}/.env`, `SKIP_PREFLIGHT_CHECK=true`)
     await shell(`cat .env`, { cwd })
     await shell(`fab init -y --skip-install --version=next`, { cwd })
-    await shell(`yarn build:fab`, { cwd })
+    const { stdout: files_after_fab_init } = await cmd(`ls -l ${cwd}`)
+    expect(files_after_fab_init).toMatch('fab.config.json5')
 
-    const { stdout: files_after_fab_build } = await cmd(`ls -l ${cwd}`)
-    expect(files_after_fab_build).toMatch('fab.zip')
-
-    await shell(`yarn add npm-run-all`, { cwd })
     const package_json = JSON.parse(await fs.readFile(`${cwd}/package.json`, 'utf8'))
     package_json.scripts = {
       ...package_json.scripts,
-      'test:fab': 'run-p --race "test:fab:*"',
-      'test:fab:serve': 'fab serve fab.zip',
-      'test:fab:test-local':
-        'curl -v --retry 3 --retry-connrefused http://localhost:3000/',
+      'fab:serve': 'fab serve fab.zip --port=3123',
     }
     await fs.writeFile(`${cwd}/package.json`, JSON.stringify(package_json, null, 2))
-
-    await shell(`yarn test:fab`, { cwd })
 
     // const { stdout: test_fab_output } = await cmd(`yarn test:fab`, { cwd })
     //
@@ -49,5 +44,31 @@ describe('Create React App E2E Test', () => {
 
     // Add a runtime plugin that returns some response
     // Then test for that response.
+  })
+
+  describe('fab build tests', () => {
+    let server_process: ExecaChildProcess | undefined
+
+    beforeEach(async () => {
+      await shell(`rm -f fab.zip`, { cwd })
+      await shell(`yarn build:fab`, { cwd })
+
+      const { stdout: files_after_fab_build } = await cmd(`ls -l ${cwd}`)
+      expect(files_after_fab_build).toMatch('fab.zip')
+
+      server_process = execa('yarn fab:serve')
+    })
+
+    it('should return a 200 on /', async () => {
+      const { stdout: first_response } = await cmd(
+        `curl -I --retry 5 --retry-connrefused http://localhost:3123/`
+      )
+
+      expect(first_response).toContain(`HTTP/1.1 200 OK`)
+    })
+
+    afterEach(async () => {
+      if (server_process) await server_process.cancel()
+    })
   })
 })
