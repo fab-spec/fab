d4ac5d36372df5fcfb8d470b85eb2a39be2341ec
Glen Maddern <glenmaddern@gmail.com>
2020-01-22 16:23:32 +0000

    CRA test now can compile FABs, spin up servers, and actually spin them down again!

--- tests/e2e/create-react-app.test.ts
+++ tests/e2e/create-react-app.test.ts
@@ -8,21 +8,31 @@ describe('Create React App E2E Test', () => {
   let tmpdir: string
   let cwd: string
 
-  it('should allow creation of a tmp dir', async () => {
-    const dir = await tmp.dir({ dir: process.env.GITHUB_WORKSPACE })
-    tmpdir = dir.path
-    await shell(`ls -l ${tmpdir}`)
-    const { stdout } = await cmd(`pwd`, { cwd: tmpdir })
-    expect(stdout).toMatch('tmp')
+  it('should allow creation of a new CRA project in a tmp dir', async () => {
+    if (process.env.FAB_E2E_CRA_DIR) {
+      cwd = process.env.FAB_E2E_CRA_DIR
+      if (!cwd.startsWith('/var/folders/' || !cwd.endsWith('/cra-test')))
+        throw new Error(`NOT TRASHING ${cwd}, SORRY.`)
+      await shell(`git reset --hard`, { cwd })
+      await shell(`git clean -df`, { cwd })
+    } else {
+      // Create the tmp dir (inside the workspace if on Github Actions)
+      const dir = await tmp.dir({ dir: process.env.GITHUB_WORKSPACE })
+      tmpdir = dir.path
+      expect(tmpdir).toMatch('tmp')
+
+      // Create a new CRA project inside
+      await shell(`yarn create react-app cra-test`, { cwd: tmpdir })
+      cwd = `${tmpdir}/cra-test`
+    }
+    // Expect that {cwd} has a package.json
+    const { stdout: files } = await cmd(`ls -l`, { cwd })
+    expect(files).toMatch('package.json')
   })
 
-  it('should allow creation of a new CRA project into a FAB', async () => {
-    await shell(`ls -l ${tmpdir}`)
-    await shell(`yarn create react-app cra-test`, { cwd: tmpdir })
-    cwd = `${tmpdir}/cra-test`
-    const { stdout: files } = await cmd(`ls -l ${cwd}`)
-    expect(files).toMatch('package.json')
-
+  it('should configure the CRA project to produce FABs', async () => {
+    // Running this on Github Actions complains because a package.json is found further
+    // up with a different version of... something. I can't remember. This fixes it tho.
     await fs.writeFile(`${cwd}/.env`, `SKIP_PREFLIGHT_CHECK=true`)
     await shell(`cat .env`, { cwd })
     await shell(`fab init -y --skip-install --version=next`, { cwd })
@@ -49,21 +59,21 @@ describe('Create React App E2E Test', () => {
   describe('fab build tests', () => {
     let server_process: ExecaChildProcess | null = null
 
-    async function cancelServer() {
+    const cancelServer = async () => {
+      console.log('CANCELLING')
+      console.log({ server_process: server_process?.constructor?.name })
       if (server_process) {
-        server_process.cancel()
-        await server_process
+        try {
+          server_process.kill()
+          await server_process
+        } catch (e) {
+          console.log('CANCELLED')
+        }
         server_process = null
       }
     }
 
-    const request = async (args: string, path: string) => {
-      const curl_cmd = `curl ${args} --retry 5 --retry-connrefused http://localhost:3123`
-      const { stdout } = await cmd(curl_cmd + path, { cwd })
-      return stdout
-    }
-
-    beforeEach(async () => {
+    const createServer = async () => {
       await cancelServer()
       await shell(`rm -f fab.zip`, { cwd })
       await shell(`yarn build:fab`, { cwd })
@@ -72,14 +82,57 @@ describe('Create React App E2E Test', () => {
       expect(files_after_fab_build).toMatch('fab.zip')
 
       server_process = cmd('yarn fab:serve', { cwd })
-    })
+    }
+
+    const request = async (args: string, path: string) => {
+      const curl_cmd = `curl ${args} --retry 5 --retry-connrefused http://localhost:3123`
+      const { stdout } = await shell(curl_cmd + path, { cwd })
+      return stdout
+    }
 
     it('should return a 200 on /', async () => {
+      await createServer()
       expect(await request('-I', '/')).toContain(`HTTP/1.1 200 OK`)
+      expect(await request('-I', '/hello')).toContain(`HTTP/1.1 200 OK`)
+
       const homepage_response = await request('', '/')
       expect(homepage_response).toContain(`<!DOCTYPE html>`)
       expect(homepage_response).toContain(`window.FAB_SETTINGS`)
       expect(homepage_response).toContain(`"__fab_server":"@fab/server"`)
+
+      const hello_response = await request('', '/hello')
+      expect(hello_response).toEqual(homepage_response)
+    })
+
+    it('should return a 200 on /', async () => {
+      await fs.ensureDir(`${cwd}/fab-plugins`)
+      await fs.writeFile(
+        `${cwd}/fab-plugins/hello-world.js`,
+        // language=JavaScript
+        `
+        export const runtime = (args, metadata) => {
+          return async ({ url }) => {
+            if (url.pathname === '/hello') {
+              return new Response('HELLO WORLD!', {
+                status: 200
+              })
+            }
+          }
+        }`
+      )
+
+      await createServer()
+      expect(await request('-I', '/')).toContain(`HTTP/1.1 200 OK`)
+      expect(await request('-I', '/hello')).toContain(`HTTP/1.1 200 OK`)
+
+      const homepage_response = await request('', '/')
+      expect(homepage_response).toContain(`<!DOCTYPE html>`)
+      expect(homepage_response).toContain(`window.FAB_SETTINGS`)
+      expect(homepage_response).toContain(`"__fab_server":"@fab/server"`)
+
+      const hello_response = await request('', '/hello')
+      expect(hello_response).not.toEqual(homepage_response)
+      expect(hello_response).toContain('HELLO WORLD!')
     })
 
     afterEach(async () => {
