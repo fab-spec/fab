6c12a81fd9b0a9c63f93987b0aa227f929919661
Glen Maddern <glenmaddern@gmail.com>
2020-01-29 17:11:51 +0000

    cherry-picking the regexp value pattern so I can refer to it in docs

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -4,3 +4,5 @@ export const DEFAULT_MIME_TYPE = 'text/html; charset=utf-8'
 
 export const IMMUTABLE_HEADERS = { 'Cache-Control': 'immutable' }
 export const NON_IMMUTABLE_HEADERS = { 'Cache-Control': 'no-cache' }
+
+export const REGEXP_VALUE_PATTERN = /^\/.*\/([gimy]*)$/
