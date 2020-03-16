b1d4de2182de1632cbd6180ac58d7258b192c109
Glen Maddern <glenmaddern@gmail.com>
2020-03-16 15:22:01 +0000

    Adding nanoid fingerprinting detection, used by NextJS for each static build_id

--- packages/rewire-assets/src/constants.ts
+++ packages/rewire-assets/src/constants.ts
@@ -1,1 +1,9 @@
-export const DEFAULT_IMMUTABILITY_CHECK = /[.-][0-9A-F]{8,}[.-]/i
+// Webpack "fingerprinting" style e.g. index.a1b2c3d4.js
+const EIGHT_OR_MORE_HEX_DIGITS = /[.-][0-9A-Fa-f]{8,}[.-]/
+
+// NanoID style (used by NextJS)
+const NANO_ID_PATTEN = /[A-Za-z0-9_-]{21,}/
+
+export const DEFAULT_IMMUTABILITY_CHECK = new RegExp(
+  [EIGHT_OR_MORE_HEX_DIGITS, NANO_ID_PATTEN].map((r) => r.source).join('|')
+)
