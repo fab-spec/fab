3a639fcd15185264a277ba736d2265419fff2a99
Glen Maddern <glenmaddern@gmail.com>
2020-03-18 17:01:49 +0000

    switching to a fork of decompress without the vulnerability

--- packages/cli/src/actions/Deployer.ts
+++ packages/cli/src/actions/Deployer.ts
@@ -7,7 +7,7 @@ import { log, short_guid } from '../helpers'
 import JSON5Config from '../helpers/JSON5Config'
 import { FabConfig } from '@fab/core'
 import shell from 'shelljs'
-import decompress from 'decompress'
+import decompress from '@atomic-reactor/decompress'
 
 interface GlobalConfig {
   aws_key: string | undefined | null
