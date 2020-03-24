896e35385e0fe916d6f89aed7fb9c1bf9c8e8c53
Glen Maddern <glenmaddern@gmail.com>
2020-03-24 19:11:29 +0000

    ready to start testing some actual functionality

--- packages/deployer-aws-lambda/src/createPackage.ts
+++ packages/deployer-aws-lambda/src/createPackage.ts
@@ -3,21 +3,33 @@ import { DEFAULT_ASSET_SETTINGS } from './constants'
 import fs from 'fs-extra'
 import path from 'path'
 import nanoid from 'nanoid'
+// @ts-ignore
+import decompress from '@atomic-reactor/decompress'
+import execa from 'execa'
 
 export const createPackage: FabPackager = async (
   fab_path: string,
   package_path: string
 ) => {
+  console.log('BOY I GON GIT')
   const asset_settings = {
     ...DEFAULT_ASSET_SETTINGS,
     // todo: parameterise asset settings?
   }
 
   const output_dir = path.dirname(package_path)
+  console.log({ output_dir })
   const work_dir = path.join(output_dir, `aws-lambda-${nanoid()}`)
+  console.log({ work_dir })
   await fs.ensureDir(work_dir)
-  // await fs.ensureDir(output_dir)
-  // await unzip_fab(fab_file, work_dir)
+  console.log('GOT ME A DIR')
+  await decompress(fab_path, work_dir)
+  console.log('DECOMPRESS YO')
+  await fs.copy(path.join(__dirname, '../templates'), work_dir)
+  console.log('COPY BOIII')
+  await execa('npm', ['install'], { cwd: work_dir })
+  console.log('NPMMMMMED')
+
   // await installNodeFetch(work_dir)
   // await fixServerPath(work_dir)
   // await writeAssetSettings(work_dir, asset_settings)
