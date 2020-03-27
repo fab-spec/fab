f090b5a482d5be542f512f237b54845fffd963da
Glen Maddern <glenmaddern@gmail.com>
2020-03-27 16:02:12 +0000

    NICE, added a linter to check Webstorm doesnt auto-import the wrong path!

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   '*.{js,css,json,md,ts}': ['prettier --write', 'git add'],
   '*.ts': [
-    () => [
-      `bash -c "grep -lr @fab/core/src packages --exclude-dir node_modules --exclude-dir lib --exclude-dir esm; test $? -eq 1"`,
-    ],
+    (files) => `bash -c "grep -l @fab/core/src ${files.join(' ')}; test $? -eq 1"`,
   ],
 }
