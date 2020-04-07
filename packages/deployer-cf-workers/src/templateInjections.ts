5ad9bd1f81aa33247c838b792703465c288d2958
Glen Maddern <glenmaddern@gmail.com>
2020-04-07 22:54:05 +0100

    got proxied requests through the FAB working ok

--- packages/deployer-cf-workers/src/templateInjections.ts
+++ packages/deployer-cf-workers/src/templateInjections.ts
@@ -1,6 +1,5 @@
 // language=JavaScript
 export default (fab_src: string, assets_url: string) => `
-  ${fab_src};
-
-  const INJECTED_ASSETS_URL = ${JSON.stringify(assets_url)};
+  ${fab_src}; // makes globalThis.__fab
+  globalThis.__assets_url = ${JSON.stringify(assets_url)};
 `
--- packages/deployer-cf-workers/templates/index.js
+++ packages/deployer-cf-workers/templates/index.js
@@ -1,5 +1,23 @@
-const FAB = __fab // injected by UMD
-const ASSETS_URL = INJECTED_ASSETS_URL // inlined in packager
+const FAB = globalThis.__fab // injected by UMD
+const ASSETS_URL = globalThis.__assets_url // inlined in packager
+const cache = caches.default
+
+globalThis.orig_fetch = globalThis.fetch
+globalThis.fetch = (url, init) => {
+  const request_url = typeof url === 'string' ? url : url.url
+  console.log({ request_url })
+
+  const makeResponseMutable = (response) => new Response(response.body, response)
+
+  if (request_url.startsWith('/')) {
+    if (!request_url.startsWith('/_assets/')) {
+      throw new Error('Fetching relative URLs for non-assets is not permitted.')
+    }
+    return orig_fetch(`${ASSETS_URL}${request_url}`, init).then(makeResponseMutable)
+  }
+
+  return orig_fetch(url, init).then(makeResponseMutable)
+}
 
 const handleRequest = async (request) => {
   const url = new URL(request.url)
