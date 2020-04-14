8c9ef39c413fcaadfb4ebc78ec8f78510417755b
Glen Maddern <glenmaddern@gmail.com>
2020-04-14 18:02:26 +0100

    deploying both to AWS Lambda seems to be working as well

--- tests/e2e/deploys.test.ts
+++ tests/e2e/deploys.test.ts
@@ -14,16 +14,27 @@ describe('Deploy tests', () => {
     {
       name: 'Create React App',
       test_dir: 'cra-test',
-      fab_filename: `fab.create-react-app.zip`,
+      fab_filename: `fab.react-app.zip`,
       cf_workers: {
-        required_env_vars: ['CF_WORKERS_API_TOKEN', 'AWS_S3_SECRET_KEY'],
+        required_env_vars: {
+          cf_api: 'CF_WORKERS_API_TOKEN',
+          s3_secret: 'AWS_S3_SECRET_KEY',
+        },
         s3_access_key: 'AKIAWIORJFSCPZGQ4K6U',
         s3_bucket: 'fab-ci-assets-create-react-app-cf-workers',
         script_name: 'fab-ci-create-react-app',
         cf_account_id: 'cd3d2f25fb7dfdd4e8be7f187b2cbad1',
       },
       aws_lambda_edge: {
-        required_env_vars: ['AWS_CRA_SECRET', 'AWS_S3_SECRET_KEY'],
+        required_env_vars: {
+          lambda_secret: 'AWS_CRA_SECRET',
+          s3_secret: 'AWS_S3_SECRET_KEY',
+        },
+        lambda_access_key: 'AKIAWIORJFSCKBFIHH3I',
+        lambda_arn:
+          'arn:aws:lambda:us-east-1:430472506500:function:create-react-app-aws-lambda-edge-renderer',
+        cf_distribution_id: 'E21MWL8EM38Z5Q',
+        s3_access_key: 'AKIAWIORJFSCPZGQ4K6U',
         s3_bucket: 'fab-ci-assets-create-react-app-aws-lambda-edge',
       },
     },
@@ -32,15 +43,26 @@ describe('Deploy tests', () => {
       test_dir: 'nextjs-test',
       fab_filename: `fab.nextjs.zip`,
       cf_workers: {
-        required_env_vars: ['CF_WORKERS_API_TOKEN', 'AWS_S3_SECRET_KEY'],
+        required_env_vars: {
+          cf_api: 'CF_WORKERS_API_TOKEN',
+          s3_secret: 'AWS_S3_SECRET_KEY',
+        },
         s3_access_key: 'AKIAWIORJFSCPZGQ4K6U',
         s3_bucket: 'fab-ci-assets-nextjs-cf-workers',
         script_name: 'fab-ci-nextjs',
         cf_account_id: 'cd3d2f25fb7dfdd4e8be7f187b2cbad1',
       },
       aws_lambda_edge: {
-        required_env_vars: ['AWS_NEXTJS_SECRET', 'AWS_S3_SECRET_KEY'],
-        s3_bucket: 'fab-ci-assets-create-react-app-aws-lambda-edge',
+        required_env_vars: {
+          lambda_secret: 'AWS_NEXTJS_SECRET',
+          s3_secret: 'AWS_S3_SECRET_KEY',
+        },
+        lambda_access_key: 'AKIAWIORJFSCGEPQ7LWJ',
+        lambda_arn:
+          'arn:aws:lambda:us-east-1:430472506500:function:nextjs-aws-lambda-edge-renderer',
+        cf_distribution_id: 'E1KZB8DX6ZCP2V',
+        s3_access_key: 'AKIAWIORJFSCPZGQ4K6U',
+        s3_bucket: 'fab-ci-assets-nextjs-aws-lambda-edge',
       },
     },
   ]
@@ -73,13 +95,44 @@ describe('Deploy tests', () => {
           deploy: {
             'cf-workers': {
               account_id: '${cf_account_id}',
-              api_token: '@${required_env_vars[0]}',
+              api_token: '@${required_env_vars.cf_api}',
               script_name: '${script_name}',
               workers_dev: true
             },
             'aws-s3': {
               access_key: '${s3_access_key}',
-              secret_key: '@${required_env_vars[1]}',
+              secret_key: '@${required_env_vars.s3_secret}',
+              region: 'us-east-1',
+              bucket_name: '${s3_bucket}',
+            }
+          }
+        }`
+
+        return await deploy(fab_filename, name, required_env_vars, test_dir, fab_config)
+      })
+
+      it('should deploy to AWS Lambda@Edge', async () => {
+        const {
+          required_env_vars,
+          lambda_access_key,
+          lambda_arn,
+          cf_distribution_id,
+          s3_access_key,
+          s3_bucket,
+        } = project.aws_lambda_edge
+        const fab_config = `{
+          plugins: {},
+          deploy: {
+            'aws-lambda-edge': {
+              access_key: '${lambda_access_key}',
+              cf_distribution_id: '${cf_distribution_id}',
+              lambda_arn: '${lambda_arn}',
+              secret_key: '@${required_env_vars.lambda_secret}',
+              region: 'us-east-1'
+            },
+            'aws-s3': {
+              access_key: '${s3_access_key}',
+              secret_key: '@${required_env_vars.s3_secret}',
               region: 'us-east-1',
               bucket_name: '${s3_bucket}',
             }
@@ -94,7 +147,7 @@ describe('Deploy tests', () => {
   async function deploy(
     fab_filename: string,
     name: string,
-    required_env_vars: string[],
+    required_env_vars: { [k: string]: string },
     test_dir: string,
     fab_config: string
   ) {
@@ -102,9 +155,10 @@ describe('Deploy tests', () => {
     if (!(await fs.pathExists(fab_path))) {
       return console.log(`No fab found at ${fab_path}. Skipping deploy test for ${name}.`)
     }
-    if (!required_env_vars.every((var_name) => process.env[var_name])) {
+    const env_vars = Object.values(required_env_vars)
+    if (!env_vars.every((var_name) => process.env[var_name])) {
       return console.log(
-        `Missing env vars. Require ${required_env_vars.join(
+        `Missing env vars. Require ${env_vars.join(
           ', '
         )}. Skipping deploy test for ${name}.`
       )
