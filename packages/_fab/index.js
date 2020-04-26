6830e97f3341858f3f80ee74bb0d508a32fbb36f
Glen Maddern <glenmaddern@gmail.com>
2020-04-26 16:17:25 +0100

    ok, time to try publishing this

---
+++ packages/_fab/index.js
@@ -0,0 +1,4 @@
+// This should never be invoked directly, since the purpose of this package is
+// to depend upon (and therefore install) the latest version of @fab/cli, which
+// declares the 'fab' binary.
+require('@fab/cli/bin/run')
