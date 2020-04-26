6e1289841f4e5e9a9ec005b8401b1128212c42ca
Glen Maddern <glenmaddern@gmail.com>
2020-04-26 16:58:52 +0100

    trying this without the actual dependency on @fab/cli

--- packages/_fab/bin/index.js
+++ packages/_fab/bin/index.js
@@ -1,3 +1,5 @@
 // This module defers to @fab/cli for all its work, we just use this namespace
 // so that `npx fab` does what we expect.
-require('@fab/cli/bin/run')
+//require('@fab/cli/bin/run')
+
+console.log('just wait one damn second')
