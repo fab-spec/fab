e9afb062d48fae869a7ab9870de7ae29da3500e6
Glen Maddern <glenmaddern@gmail.com>
2020-04-26 17:21:32 +0100

    shebang required perhaps?

--- packages/_fab/fab.js
+++ packages/_fab/fab.js
@@ -1,5 +1,5 @@
+#!/usr/bin/env node
+
 // This module defers to @fab/cli for all its work, we just use this namespace
 // so that `npx fab` does what we expect.
-//require('@fab/cli/bin/run')
-
-console.log('just wait one damn second')
+require('@fab/cli/bin/run')
