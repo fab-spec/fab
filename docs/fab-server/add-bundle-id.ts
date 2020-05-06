6539931dccf7926818e00fb50319fa8b833af038
Glen Maddern <glenmaddern@gmail.com>
2020-05-06 17:20:21 +0100

    adding @fab/add-fab-id as a plugin

--- docs/fab-server/add-bundle-id.ts
+++
@@ -1,8 +0,0 @@
-import { FABRuntime } from '@fab/core'
-
-export default ({ Router, ServerContext }: FABRuntime) => {
-  Router.interceptResponse(async (response) => {
-    response.headers.set('X-FAB-ID', ServerContext.bundle_id)
-    return response
-  })
-}
