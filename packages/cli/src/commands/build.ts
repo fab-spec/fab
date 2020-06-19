0a1e5dad4614e57c0cb15fe61c9a5cc86103afa0
Glen Maddern <glenmaddern@gmail.com>
2020-06-19 18:44:50 +0100

    allowing the --watch flag to rebuild on changes to whatever files you want

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -1,6 +1,7 @@
 import { Command, flags } from '@oclif/command'
 import { DEFAULT_CONFIG_FILENAME, FabActionsExports } from '@fab/core'
 import { JSON5Config } from '../'
+import chokidar from 'chokidar'
 
 export default class Build extends Command {
   static description = 'Generate a FAB given the config (usually in fab.config.json5)'
@@ -26,9 +27,34 @@ export default class Build extends Command {
 
   async run() {
     const { args, flags } = this.parse(Build)
-    console.log(flags.watch)
     const config = await JSON5Config.readFrom(flags.config!)
     const { Builder } = require('@fab/actions').default as FabActionsExports
-    // await Builder.build(flags.config, config.data, flags['skip-cache'])
+    const watch = flags.watch || []
+
+    const build = async () => {
+      await Builder.build(flags.config, config.data, flags['skip-cache'])
+    }
+
+    if (watch.length > 0) {
+      let building = false
+
+      const rebuild = async (message: string) => {
+        if (building) return
+        building = true
+        console.clear()
+        console.log(message)
+        await build()
+        building = false
+      }
+
+      rebuild(`Watching paths: ${watch.join(' ')}`)
+      chokidar.watch(watch).on('ready', () => {
+        chokidar.watch(watch).on('all', (event, path) => {
+          rebuild(`${path} changed`)
+        })
+      })
+    } else {
+      await build()
+    }
   }
 }
