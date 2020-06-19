399bb3009ab87591c8d211c96a37491e492afa06
Glen Maddern <glenmaddern@gmail.com>
2020-06-19 23:17:14 +0100

    leaving notes for me for the morning

--- packages/plugin-node-compat/src/build.ts
+++ packages/plugin-node-compat/src/build.ts
@@ -8,5 +8,13 @@ export async function build(
   args: NodeCompatArgs,
   proto_fab: ProtoFab<NodeCompatMetadata>
 ) {
+  /*
+   * TODO
+   *
+   * Port the @fab/input-nextjs stuff across, shimming out pieces as needed.
+   * Make each file a new "runtime", that's compiled using webpack, with a bunch
+   * of good defaults but the args for each section are all the overrides.
+   *
+   * */
   return Object.keys(args)
 }
