967d0a735abdf2e8e67e8e2049c54031215569ba
Glen Maddern <glenmaddern@gmail.com>
2020-06-19 23:15:33 +0100

    attempting to invalidate node cache for reloading but it no work

--- packages/cli/src/helpers/watcher.ts
+++ packages/cli/src/helpers/watcher.ts
@@ -1,4 +1,5 @@
 import chokidar, { WatchOptions } from 'chokidar'
+import p from 'path'
 
 export const watcher = async (
   dirs: string[] | undefined,
@@ -7,12 +8,20 @@ export const watcher = async (
 ) => {
   if (dirs && dirs.length > 0) {
     let building = false
+    const invalidations: string[] = []
 
     const run_fn_once_at_a_time = async (message: string) => {
       if (building) return
       building = true
-      console.clear()
+      // console.clear()
       console.log(message)
+      console.log(Object.keys(require.cache).filter((x) => x.match(/plugin-node-compat/)))
+      invalidations.forEach((path) => {
+        console.log(`Invalidating ${path}`)
+        delete require.cache[path]
+      })
+      console.log(Object.keys(require.cache).filter((x) => x.match(/plugin-node-compat/)))
+      invalidations.length = 0
       await fn()
       building = false
     }
@@ -22,6 +31,17 @@ export const watcher = async (
 
     chokidar.watch(dirs).on('ready', () => {
       chokidar.watch(dirs, options).on('all', (event, path) => {
+        console.log({ event, path })
+        if (event === 'change') {
+          console.log('trying to resolve')
+          try {
+            const resolved_path = p.resolve(path)
+            console.log({ resolved_path })
+            invalidations.push(resolved_path)
+          } catch (e) {
+            // noop
+          }
+        }
         run_fn_once_at_a_time(`${path} changed`)
       })
     })
