d595896bca2f09984b6d8c637d7de65eb146204c
Glen Maddern <glenmaddern@gmail.com>
2020-06-20 10:03:58 +0100

    got webpack running, but lol of course ES modules break stuff

--- packages/plugin-node-compat/src/build.ts
+++ packages/plugin-node-compat/src/build.ts
@@ -1,13 +1,19 @@
-import { ProtoFab } from '@fab/core'
+import { FabBuildStep, ProtoFab } from '@fab/core'
 import { NodeCompatArgs, NodeCompatMetadata } from './types'
 import { _log } from '@fab/cli'
+import webpack from 'webpack'
+import fs from 'fs-extra'
+import path from 'path'
+import filenamify from 'filenamify'
 
 const log = _log('@fab/plugin-node-compat')
 
-export async function build(
+export const build: FabBuildStep<NodeCompatArgs, NodeCompatMetadata> = async (
   args: NodeCompatArgs,
-  proto_fab: ProtoFab<NodeCompatMetadata>
-) {
+  proto_fab: ProtoFab<NodeCompatMetadata>,
+  config_path,
+  skip_cache = false
+) => {
   /*
    * TODO
    *
@@ -16,5 +22,68 @@ export async function build(
    * of good defaults but the args for each section are all the overrides.
    *
    * */
-  return Object.keys(args)
+  const config_dir = path.dirname(path.resolve(config_path))
+  const cache_dir = path.join(config_dir, '.fab', '.cache', 'node-compat')
+  await fs.ensureDir(cache_dir)
+
+  const output_files: string[] = []
+
+  for (const [file, config] of Object.entries(args)) {
+    const webpacked_output = path.join(
+      cache_dir,
+      `${filenamify(file, { replacement: '-' })}.js`
+    )
+    const entry = path.resolve(file)
+    await new Promise((resolve, reject) =>
+      webpack(
+        {
+          stats: 'verbose',
+          mode: 'production',
+          target: 'webworker',
+          entry,
+          optimization: {
+            minimize: false,
+          },
+          output: {
+            path: path.dirname(webpacked_output),
+            filename: path.basename(webpacked_output),
+            library: 'server',
+            libraryTarget: 'commonjs2',
+          },
+          resolve: {
+            alias: {
+              fs: require.resolve('memfs'),
+              // path: path.join(shims_dir, 'path-with-posix'),
+              // '@ampproject/toolbox-optimizer': path.join(shims_dir, 'empty-object'),
+              // http: path.join(shims_dir, 'http'),
+              // https: path.join(shims_dir, 'empty-object'),
+            },
+          },
+          node: {
+            global: false,
+          },
+          plugins: [
+            /* Cloudflare Workers will explode if it even _sees_ `eval` in a file,
+             * even if it's never called. Replacing it with this will bypasses that.
+             * (It'll still explode if it's called, nothing we can do about that.) */
+            new webpack.DefinePlugin({
+              eval: 'HERE_NO_EVAL',
+            }),
+          ],
+        },
+        (err, stats) => {
+          if (err || stats.hasErrors()) {
+            console.log('Build failed.')
+            console.log(err)
+            console.log(stats && stats.toJson().errors.toString())
+            reject()
+          }
+          resolve()
+        }
+      )
+    )
+    output_files.push(webpacked_output)
+  }
+
+  return output_files
 }
