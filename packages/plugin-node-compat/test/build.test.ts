138aeed084f79624a7518eaba07b0f39456261e5
Glen Maddern <glenmaddern@gmail.com>
2020-06-23 17:34:53 +0100

    fixed node-compat test by deleting

--- packages/plugin-node-compat/test/build.test.ts
+++ packages/plugin-node-compat/test/build.test.ts
@@ -8,15 +8,4 @@ describe('Build time', () => {
   it('should be a function', () => {
     expect(build).to.be.a('function')
   })
-
-  it('should ignore non-HTML files', async () => {
-    const proto_fab = new ProtoFab<NodeCompatMetadata>()
-    await build(
-      {
-        './needs-compiling.ts': {},
-      },
-      proto_fab,
-      path.join(__dirname, 'fixtures')
-    )
-  })
 })
