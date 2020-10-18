e651cad713a854b731510ee0808e42dac74a700a
Erwin van der Koogh <git@koogh.com>
2020-10-18 18:33:11 +1100

    Major refactoring of Server to make it more readable.

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,7 +2,7 @@ import fs from 'fs-extra'
 
 import {
   FabServerExports,
-  FetchApi,
+  FabSpecExports,
   getContentType,
   SandboxType,
   ServerArgs,
@@ -16,7 +16,11 @@ import { Cache } from './cache'
 import node_vm_sandbox from '@fab/sandbox-node-vm'
 import url from 'url'
 import http from 'http'
-import express, { Express } from 'express'
+import express, {
+  Express,
+  Request as ExpressRequest,
+  Response as ExpressResponse,
+} from 'express'
 import concat from 'concat-stream'
 import fetch, { Request as NodeFetchRequest } from 'cross-fetch'
 import { pathToSHA512 } from 'file-to-sha512'
@@ -32,6 +36,56 @@ function isRequest(fetch_res: Request | Response): fetch_res is Request {
   )
 }
 const log = _log(`Server`)
+
+async function streamResponse(fetch_res: Response, res: ExpressResponse) {
+  res.status(fetch_res.status)
+  // This is a NodeFetch response, which has this method, but
+  // the @fab/core types are from dom.ts, which doesn't. This
+  // was the easiest workaround for now.
+  // @ts-ignore
+  const response_headers = fetch_res.headers.raw()
+  delete response_headers['content-encoding']
+  Object.keys(response_headers).forEach((header) => {
+    const values = response_headers[header]
+    res.set(header, values.length === 1 ? values[0] : values)
+  })
+
+  const body = fetch_res.body
+  if (body) {
+    if (typeof body.getReader === 'function') {
+      if (!response_headers['transfer-encoding']) res.set('transfer-encoding', 'chunked')
+
+      const reader = body.getReader()
+      let x
+      while ((x = await reader.read())) {
+        const { done, value } = x
+        if (done) break
+        if (value) {
+          if (typeof value === 'string') {
+            res.write(value)
+          } else {
+            res.write(Buffer.from(value))
+          }
+        }
+      }
+      res.end()
+    } else if (body instanceof Stream) {
+      if (!response_headers['transfer-encoding']) res.set('transfer-encoding', 'chunked')
+
+      await new Promise((resolve, reject) => {
+        body.on('data', (chunk) => res.write(chunk))
+        body.on('error', reject)
+        body.on('end', resolve)
+      })
+      res.end()
+    } else {
+      const blob = await fetch_res.arrayBuffer()
+      res.send(Buffer.from(blob))
+    }
+  } else {
+    res.end()
+  }
+}
 
 class Server implements ServerType {
   filename: string
@@ -52,6 +106,189 @@ class Server implements ServerType {
     this.env = args.env
   }
 
+  private async enchanched_fetch(
+    url: RequestInfo,
+    init?: RequestInit
+  ): Promise<Response> {
+    const request_url = typeof url === 'string' ? url : url.url
+    const response = await fetch(
+      request_url.startsWith('/')
+        ? // Need a smarter way to re-enter the FAB, eventually...
+          `http://localhost:${this.port}${request_url}`
+        : url,
+      init
+    )
+    return Object.create(response, {
+      body: {
+        value: Object.create(response.body, {
+          getReader: {
+            get() {
+              const webStream = nodeToWebStream(response.body)
+              return webStream.getReader.bind(webStream)
+            },
+          },
+        }),
+      },
+    })
+  }
+
+  private async createRenderer(
+    src: string,
+    runtimeType: SandboxType
+  ): Promise<FabSpecExports> {
+    const renderer =
+      (await runtimeType) === SandboxType.v8isolate
+        ? await v8_sandbox(src)
+        : await node_vm_sandbox(src, this.enchanched_fetch)
+
+    const bundle_id = (await pathToSHA512(this.filename)).slice(0, 32)
+    const cache = new Cache()
+    // Support pre v0.2 FABs
+    if (typeof renderer.initialize === 'function') {
+      renderer.initialize({ bundle_id, cache })
+    }
+
+    return renderer
+  }
+
+  private async renderReq(
+    renderer: FabSpecExports,
+    req: ExpressRequest,
+    settings_overrides: any
+  ): Promise<Response> {
+    const method = req.method
+    const headers: any = req.headers
+    const url = `${req.protocol}://${req.headers.host}${req.url}`
+    const fetch_req = new NodeFetchRequest(url, {
+      method,
+      headers,
+      ...(method === 'POST' ? { body: req.body } : {}),
+    })
+
+    const production_settings = renderer.metadata?.production_settings
+    let fetch_res
+    try {
+      fetch_res = await renderer.render(
+        // @ts-ignore
+        fetch_req as Request,
+        Object.assign({}, production_settings, settings_overrides)
+      )
+    } catch (err) {
+      const msg = `An error occurred calling the render method on the FAB: \nError: \n${err}`
+      throw new Error(msg)
+    }
+    try {
+      if (fetch_res && isRequest(fetch_res)) {
+        fetch_res = await this.enchanched_fetch(fetch_res)
+      }
+    } catch (err) {
+      const msg = `An error occurred proxying a request returned from the FAB: \nError:\n${err}\nRequest:\n${fetch_res}`
+      throw new Error(msg)
+    }
+
+    if (!fetch_res) {
+      const msg = `Nothing was returned from the FAB renderer.`
+      throw new Error(msg)
+    }
+    return fetch_res
+  }
+
+  private setupExpress(
+    renderer: FabSpecExports,
+    settings_overrides: any,
+    files: { [filename: string]: Buffer }
+  ): Express {
+    const app = express()
+
+    app.use((req, res, next) => {
+      try {
+        next()
+      } catch (err) {
+        log(`ERROR serving: ${req.url}`)
+        log(err)
+        if (!res.headersSent) {
+          res.writeHead(500, `Internal Error:\n${err}`)
+        }
+        res.end()
+      }
+    })
+
+    app.use((req, _res, next) => {
+      req.pipe(
+        concat((data: any) => {
+          req.body = data.toString()
+          next()
+        })
+      )
+    })
+
+    app.use((req, _res, next) => {
+      log(`🖤${req.url}🖤`)
+      next()
+    })
+
+    app.get('/_assets/*', (req, res) => {
+      const pathname = url.parse(req.url!).pathname!
+      res.setHeader('Content-Type', getContentType(pathname))
+      res.setHeader('Cache-Control', 'immutable')
+      res.end(files[pathname])
+    })
+
+    app.all('*', async (req, res) => {
+      const fetch_res = await this.renderReq(renderer, req, settings_overrides)
+      streamResponse(fetch_res, res)
+    })
+
+    return app
+  }
+
+  private async bootServer(runtimeType: SandboxType, proxyWs: string | undefined) {
+    let proxy: any
+    let server: ReturnType<typeof http.createServer>
+
+    log(`Reading 💛${this.filename}💛...`)
+    const files = await readFilesFromZip(this.filename)
+    const src_buffer = files['/server.js']
+    if (!src_buffer) {
+      throw new FabServerError('Malformed FAB. Missing /server.js')
+    }
+    const src = src_buffer.toString('utf8')
+
+    log.tick(`Done. Booting VM...`)
+    const settings_overrides = await this.getSettingsOverrides()
+    const renderer = await this.createRenderer(src, runtimeType)
+
+    log.tick(`Done. Booting FAB server...`)
+
+    await new Promise((resolve, _reject) => {
+      const app = this.setupExpress(renderer, settings_overrides, files)
+      if (!server) {
+        server = http.createServer((req, res) => app(req, res))
+
+        if (proxyWs) {
+          if (!proxy) {
+            proxy = httpProxy.createProxyServer({
+              target: `ws://localhost:${proxyWs}`,
+              ws: true,
+            })
+          }
+          //   ? https.createServer({ key: this.key, cert: this.cert }, app)
+
+          server.on('upgrade', (req, socket, head) => {
+            proxy.ws(req, socket, head)
+          })
+        }
+
+        server.listen(this.port, resolve)
+      } else {
+        resolve()
+      }
+    })
+
+    log.tick(`Done.`)
+    log(`Listening on 💛http://localhost:${this.port}💛`)
+  }
+
   async serve(
     runtimeType: SandboxType,
     watching: boolean = false,
@@ -61,216 +298,16 @@ class Server implements ServerType {
       throw new FabServerError(`Could not find file '${this.filename}'`)
     }
 
-    let app: Express
-    let proxy: any
-    let server: ReturnType<typeof http.createServer>
-
-    const bootServer = async () => {
-      log(`Reading 💛${this.filename}💛...`)
-      const files = await readFilesFromZip(this.filename)
-      log.tick(`Done. Booting FAB server...`)
-
-      const settings_overrides = await this.getSettingsOverrides()
-
-      const src_buffer = files['/server.js']
-      if (!src_buffer) {
-        throw new FabServerError('Malformed FAB. Missing /server.js')
-      }
-      const src = src_buffer.toString('utf8')
-
-      const enhanced_fetch: FetchApi = async (url, init?) => {
-        const request_url = typeof url === 'string' ? url : url.url
-        const response = await fetch(
-          request_url.startsWith('/')
-            ? // Need a smarter wau to re-enter the FAB, eventually...
-              `http://localhost:${this.port}${request_url}`
-            : url,
-          init
-        )
-        return Object.create(response, {
-          body: {
-            value: Object.create(response.body, {
-              getReader: {
-                get() {
-                  const webStream = nodeToWebStream(response.body)
-                  return webStream.getReader.bind(webStream)
-                },
-              },
-            }),
-          },
-        })
-      }
-
-      const renderer =
-        (await runtimeType) === SandboxType.v8isolate
-          ? await v8_sandbox(src)
-          : await node_vm_sandbox(src, enhanced_fetch)
-
-      const bundle_id = (await pathToSHA512(this.filename)).slice(0, 32)
-      const cache = new Cache()
-      // Support pre v0.2 FABs
-      if (typeof renderer.initialize === 'function')
-        renderer.initialize({ bundle_id, cache })
-
-      log.tick(`Done. Booting VM...`)
-
-      await new Promise((resolve, reject) => {
-        app = express()
-
-        app.use((req, res, next) => {
-          req.pipe(
-            concat((data: any) => {
-              req.body = data.toString()
-              next()
-            })
-          )
-        })
-
-        app.all('*', async (req, res) => {
-          try {
-            const pathname = url.parse(req.url!).pathname!
-            if (pathname.startsWith('/_assets')) {
-              res.setHeader('Content-Type', getContentType(pathname))
-              res.setHeader('Cache-Control', 'immutable')
-              res.end(files[pathname])
-            } else {
-              const method = req.method
-              const headers: any = req.headers
-              const url = `${req.protocol}://${req.headers.host}${req.url}`
-              const fetch_req = new NodeFetchRequest(url, {
-                method,
-                headers,
-                ...(method === 'POST' ? { body: req.body } : {}),
-              })
-
-              const production_settings = renderer.metadata?.production_settings
-              let fetch_res
-              try {
-                fetch_res = await renderer.render(
-                  // @ts-ignore
-                  fetch_req as Request,
-                  Object.assign({}, production_settings, settings_overrides)
-                )
-              } catch (err) {
-                const msg = `An error occurded calling the render method on the FAB: \nError: \n${err}`
-                console.error(msg)
-                return res.status(500).send(msg)
-              }
-              try {
-                if (fetch_res && isRequest(fetch_res)) {
-                  fetch_res = await enhanced_fetch(fetch_res)
-                }
-              } catch (err) {
-                const msg = `An error occurded proxying a request returned from the FAB: \nError:\n${err}\nRequest:\n${fetch_res}`
-                console.error(msg)
-                return res.status(500).send(msg)
-              }
-
-              if (!fetch_res) {
-                const msg = `Nothing was returned from the FAB renderer.`
-                console.error(msg)
-                return res.status(500).send(msg)
-              }
-              res.status(fetch_res.status)
-              // This is a NodeFetch response, which has this method, but
-              // the @fab/core types are from dom.ts, which doesn't. This
-              // was the easiest workaround for now.
-              // @ts-ignore
-              const response_headers = fetch_res.headers.raw()
-              // console.log({response_headers})
-              delete response_headers['content-encoding']
-              Object.keys(response_headers).forEach((header) => {
-                const values = response_headers[header]
-                res.set(header, values.length === 1 ? values[0] : values)
-              })
-
-              const body = fetch_res.body
-              if (body) {
-                if (typeof body.getReader === 'function') {
-                  if (!response_headers['transfer-encoding'])
-                    res.set('transfer-encoding', 'chunked')
-
-                  const reader = body.getReader()
-                  let x
-                  while ((x = await reader.read())) {
-                    const { done, value } = x
-                    if (done) break
-                    if (value) {
-                      if (typeof value === 'string') {
-                        res.write(value)
-                      } else {
-                        res.write(Buffer.from(value))
-                      }
-                    }
-                  }
-                  res.end()
-                } else if (body instanceof Stream) {
-                  if (!response_headers['transfer-encoding'])
-                    res.set('transfer-encoding', 'chunked')
-
-                  await new Promise((resolve, reject) => {
-                    body.on('data', (chunk) => res.write(chunk))
-                    body.on('error', reject)
-                    body.on('end', resolve)
-                  })
-                  res.end()
-                } else {
-                  const blob = await fetch_res.arrayBuffer()
-                  res.send(Buffer.from(blob))
-                }
-              } else {
-                res.end()
-              }
-            }
-          } catch (e) {
-            console.log('ERROR')
-            console.log(e)
-            if (!res.headersSent) {
-              res.writeHead(500, 'Internal Error')
-            }
-            res.end()
-          }
-
-          log(`🖤${req.url}🖤`)
-        })
-
-        if (!server) {
-          if (watching) log.note(`Watching 💛${this.filename}💛 for changes...`)
-          server = http.createServer((req, res) => app(req, res))
-
-          if (proxyWs) {
-            if (!proxy) {
-              proxy = httpProxy.createProxyServer({
-                target: `ws://localhost:${proxyWs}`,
-                ws: true,
-              })
-            }
-            //   ? https.createServer({ key: this.key, cert: this.cert }, app)
-
-            server.on('upgrade', (req, socket, head) => {
-              proxy.ws(req, socket, head)
-            })
-          }
-
-          server.listen(this.port, resolve)
-        } else {
-          resolve()
-        }
-      })
-
-      log.tick(`Done.`)
-      log(`Listening on 💛http://localhost:${this.port}💛`)
-    }
-
     if (watching) {
-      await watcher([this.filename], bootServer, {
+      log.note(`Watching 💛${this.filename}💛 for changes...`)
+      await watcher([this.filename], () => this.bootServer(runtimeType, proxyWs), {
         awaitWriteFinish: {
           stabilityThreshold: 200,
           pollInterval: 50,
         },
       })
     } else {
-      await bootServer()
+      await this.bootServer(runtimeType, proxyWs)
     }
   }
 
