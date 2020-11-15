1a8743800530e0da6e32400fe4ff9d54032ec29b
Glen Maddern <glenmaddern@gmail.com>
2020-11-15 15:43:18 +0000

    trying everything i can think of

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -1,5 +1,5 @@
 jest.setTimeout(300000) // 5 minutes in milliseconds
-const failFast = require('jasmine-fail-fast')
-
-const jasmineEnv = jasmine.getEnv()
-jasmineEnv.addReporter(failFast.init())
+//const failFast = require('jasmine-fail-fast')
+//
+//const jasmineEnv = jasmine.getEnv()
+//jasmineEnv.addReporter(failFast.init())
--- tests/utils.ts
+++ tests/utils.ts
@@ -1,18 +1,19 @@
 import * as execa from 'execa'
 
-export const cmd = (command: string, opts: any) => {
+export const cmd = (command: string, opts?: any) => {
   process.stdout.write(`$ ${command}\n`)
   return execa.command(command, opts)
 }
 
-export const shell = (command: string, opts: any) => {
-  if (opts.cwd) process.stdout.write(`[${opts.cwd}] `)
+export const shell = (command: string, opts?: any) => {
+  if (opts?.cwd) process.stdout.write(`[${opts.cwd}] `)
   const promise = cmd(command, opts)
   promise.stdout!.pipe(process.stdout)
   promise.stderr!.pipe(process.stderr)
   return promise
 }
-export const _shell = (cwd: string) => (command: string, opts: any) =>
+
+export const _shell = (cwd: string) => (command: string, opts?: any) =>
   shell(command, { cwd, ...opts })
 
 const SHOULD_HAVE_THROWN = `Shouldn't get here, expected to have already thrown`
