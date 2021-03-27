c3b1a4b1be701eb769c83b289316a41b0ece8b7e
Abdelhamid Attaby <44318181+abdelhamid-attaby@users.noreply.github.com>
2021-03-27 20:18:10 +0200

    Update build.ts to execute webpack command

--- packages/input-flareact/src/build.ts
+++ packages/input-flareact/src/build.ts
@@ -1,7 +1,8 @@
 import { FabBuildStep } from '@fab/core'
 import { InputFlareactMetadata, InputFlareactArgs } from './types'
-import fs from 'fs-extra'
+import path from 'path'
 import { relativeToConfig } from '@fab/cli'
+import webpack from 'webpack'
 
 export const build: FabBuildStep<InputFlareactArgs, InputFlareactMetadata> = async (
   args,
@@ -10,8 +11,26 @@ export const build: FabBuildStep<InputFlareactArgs, InputFlareactMetadata> = asy
   skip_cache = false
 ) => {
   /* NO IDEA IF THESE MAKE SENSE TO BE CONFIGURABLE JUST AN EXAMPLE */
+  const config_dir = path.dirname(path.resolve(config_path))
+  const webpack_worker_config_file_path = path.join(
+    config_dir,
+    'node_modules/flareact/webpack'
+  )
+  const get_webpack_config = require(webpack_worker_config_file_path)
+  const webpack_config = get_webpack_config()
 
-  const { static_dir = '_out', worker_path = 'worker/script.js' } = args
+  await new Promise((resolve, reject) =>
+    webpack(webpack_config, (err, stats) => {
+      if (err || stats?.hasErrors()) {
+        console.log('Build failed.')
+        console.log(err)
+        console.log(stats && stats?.toJson()?.errors?.toString())
+        reject()
+      }
+      resolve(null)
+    })
+  )
+  const { static_dir = '_out', worker_path = 'dist/main.js' } = args
 
   const abs_static_dir = relativeToConfig(config_path, static_dir, false)
   const abs_worker_path = relativeToConfig(config_path, worker_path, false)
